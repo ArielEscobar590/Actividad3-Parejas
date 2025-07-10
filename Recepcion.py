a03004ce671db82b36beaa7b2713f76f90fcc4d5
German Escobar <germanjosearielescobarescobar@gmail.com>
2025-07-09 18:47:35 -0600

    Clinica en Parejas

--- Recepcion.py
+++ Recepcion.py
@@ -10,4 +10,12 @@ class Cliente:
 
     def listaClientes(self):
         for cliente in self.clientes:
-            print(f"{cliente.nombre}, {cliente.edad}, {cliente.padeciemiento}")
+            a = 1
+            print(f"{a}) {cliente.nombre}, {cliente.edad}, {cliente.padeciemiento}")
+            a += 1
+
+def main():
+    nombre = input("Ingrese el nombre del cliente: ")
+    edad = input("Ingrese el edad del cliente: ")
+    padeciemiento = input("Ingrese el padeciemiento del cliente: ")
+    cliente = Cliente(nombre, edad, padeciemiento)
