056e931542bc2d9f1bf34d21b8549282e4fa3551
Usuario <hugocanizzabala@gmail.com>
2025-07-09 18:48:19 -0600

    despliegue de medicamento

--- farmacia.py
+++ farmacia.py
@@ -1,5 +1,16 @@
 
 pila_farmacia =[]
-medicina=string(imput("ingrese lista de medicamento"))
-pila_farmacia.append(medicina)
+cola_medicamento=[]
+medicina =""
+while medicina !="salir":
+   medicina=string(imput("ingrese lista de medicamento"))
+   pila_farmacia.append(medicina)
+   print("ingreso con éxito")
 
+for i in pila_farmacia:
+    cola_medicamento.append(i)
+
+print("medicamento ingresado")
+print(pila_farmacia)
+print("despacho de medicamento")
+print(cola_medicamento)
